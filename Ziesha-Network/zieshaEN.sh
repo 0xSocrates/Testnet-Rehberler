f3c3d28c39b02b4620caa2021778ea6816148330
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-04-11 00:45:40 +0300

    Update zieshaEN.sh

--- Ziesha-Network/zieshaEN.sh
+++ Ziesha-Network/zieshaEN.sh
@@ -104,7 +104,7 @@ if [ $CUZDAN == "1" ]; then
  echo -e "\e[0;34mBazuka is Initialized.\033[0m"
  echo ""
  sleep 2
- bazuka init --external $(wget -qO- eth0.me):8765 --bootstrap 31.210.53.186:8765 --bootstrap 165.22.212.230:8765 --mnemonic "$MNEMONIC"
+ bazuka init --external $(wget -qO- eth0.me):8765 --bootstrap 31.210.53.186:8765 --bootstrap 165.22.212.230:8765 --bootstrap 31.210.53.186:8765 --mnemonic "$MNEMONIC"
  sleep 3
   elif [ $CUZDAN == "2" ]; then
    sleep 1
@@ -120,7 +120,7 @@ if [ $CUZDAN == "1" ]; then
    echo ""
    echo ""
    sleep 3
-   bazuka init --external $(wget -qO- eth0.me):8765 --bootstrap 31.210.53.186:8765 --bootstrap 165.22.212.230:8765
+   bazuka init --external $(wget -qO- eth0.me):8765 --bootstrap 31.210.53.186:8765 --bootstrap 165.22.212.230:8765 --bootstrap 31.210.53.186:8765
    sleep 8
 fi 
 
@@ -154,7 +154,7 @@ echo -e ""
 sleep 1
 echo -e "\033[33m"
 echo -e "- You must wait to sync before making transactions."
-echo -e "- Check your node in Explorer.  http://65.108.193.133:8000/"
+echo -e "- Check your node in Explorer.  http://31.210.53.186:8000/"
 echo -e "- Don't forget to follow Discord for updates. If you have any questions you can ask on discord."
 sleep 2
 echo -e ""
