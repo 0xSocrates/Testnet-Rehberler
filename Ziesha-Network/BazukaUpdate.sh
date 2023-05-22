27829a3f72c7b2f46b8110d2d93dbbe4f6f095e7
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-05-22 15:48:26 +0300

    Create BazukaUpdate.sh

---
+++ Ziesha-Network/BazukaUpdate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+clear
+sleep 1
+echo -e '\e[0;35m'
+echo " +-+-+-+-+-+-+-+-+"
+echo " |S|o|c|r|a|t|e|s|"
+echo " +-+-+-+-+-+-+-+-+"
+echo -e '\e[0m'
+sleep 1 
+echo "\e[0;34mBazuka Update Script\033[0m"
+echo ""
+echo ""
+sudo systemctl stop bazuka
+echo -e '\e[0;32m' && echo "Bazuka Node Stopped" && echo -e '\e[0m'
+cd bazuka
+git pull origin master
+cargo update
+sleep 1 
+echo -e '\e[0;32m' && echo "Updating Bazuka Version" && echo -e '\e[0m'
+cargo install --path .
+sleep 1
+echo -e '\e[0;32m' && echo "Bazuka Version Updated" && echo -e '\e[0m'
+sudo systemctl start bazuka
+sudo systemctl restart bazuka
+sleep 1
+echo -e '\e[0;32m' && echo "Node Started" && echo -e '\e[0m'
+sleep 2
+clear
+echo -e '\e[0;32m' && echo "Here are the logs" && echo -e '\e[0m'
+sleep 1
+sudo journalctl -u bazuka -fo cat
