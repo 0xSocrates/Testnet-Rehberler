006ddc90ac68e7fcadae9c56e9277026d568f728
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-05-22 15:56:49 +0300

    Update BazukaUpdate.sh

--- Ziesha-Network/BazukaUpdate.sh
+++ Ziesha-Network/BazukaUpdate.sh
@@ -6,8 +6,9 @@ echo " +-+-+-+-+-+-+-+-+"
 echo " |S|o|c|r|a|t|e|s|"
 echo " +-+-+-+-+-+-+-+-+"
 echo -e '\e[0m'
-sleep 1 
+sleep 2 
 echo -e "\e[0;34mBAZUKA UPDATE SCRIPT\033[0m"
+slepp 1
 echo ""
 echo ""
 sudo systemctl stop bazuka
@@ -25,7 +26,6 @@ sudo systemctl restart bazuka
 sleep 1
 echo -e '\e[0;32m' && echo "Node Started" && echo -e '\e[0m'
 sleep 2
-clear
 echo -e '\e[0;32m' && echo "Here are the logs" && echo -e '\e[0m'
 sleep 1
 sudo journalctl -u bazuka -fo cat
