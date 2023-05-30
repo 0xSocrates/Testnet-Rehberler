c235f91a7493f281fc6a61b5d318d83c752da376
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-05-30 20:06:42 +0300

    Update empower-kurulum.sh

--- Empower Chain/empower-kurulum.sh
+++ Empower Chain/empower-kurulum.sh
@@ -107,6 +107,8 @@ seeds=" "
 peers=" "
 sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.empowerchain/config/config.toml
 sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.025umpwr\"/" $HOME/.empowerchain/config/app.toml
+sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:15058\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:15057\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:15060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:15056\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":15066\"%" $HOME/.empowerchain/config/config.toml
+sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:15017\"%; s%^address = \":8080\"%address = \":15080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:15090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:15091\"%; s%:8545%:15045%; s%:8546%:15046%; s%:6065%:15065%" $HOME/.empowerchain/config/app.toml
 exec > /dev/tty 2>&1
 echo -e '\e[0;32m'
 echo -e "Min gas price ✔"
@@ -118,6 +120,8 @@ sleep 1
 echo -e "Genesis ✔"
 sleep 1
 echo -e "Addrbook ✔"
+sleep 1
+echo -e "Port ✔"
 echo -e '\e[0m'
 
 exec > /dev/null 2>&1
