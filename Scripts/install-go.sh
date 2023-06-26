2d8f586b9ea811c9557a23bd6571d6647d1997bc
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-06-26 15:37:32 +0300

    Create install-go.sh

---
+++ Scripts/install-go.sh
@@ -0,0 +1,8 @@
+cd $HOME
+wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
+rm -rf go1.20.4.linux-amd64.tar.gz
